08f9df005d2176a85011ad1ea2ca0f8b56f0fece
msl <robin@mrrobinsmith.com>
2013-11-16 15:15:50 +0000

    Auto-update

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,3 +1,2 @@
 class BlogPost < ActiveRecord::Base
-
 end
--- db/migrate/20131116130720_create_blog_posts.rb
+++ db/migrate/20131116130720_create_blog_posts.rb
@@ -1,6 +1,11 @@
 class CreateBlogPosts < ActiveRecord::Migration
   def change
     create_table :blog_posts do |t|
+      t.string :title
+      t.text :body
+      t.date :date
+      t.boolean :published
+      t.string :author
 
       t.timestamps
     end
