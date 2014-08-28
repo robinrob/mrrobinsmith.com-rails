751ff05ccc2ac6ec0ebf226dc4a4e9a28cc46fc0
Robin Smith <robin.smith@cloudreach.co.uk>
2014-08-28 15:53:11 +0100

    Auto-update

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ FirstApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#home'
+  root 'blogposts#index'
 
   get '/' => 'pages#home'
 
