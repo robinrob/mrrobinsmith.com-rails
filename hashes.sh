5819a44c369c9b9b5b20ccdc167355b5efd8f9a4
Robin Smith <robin@mrrobinsmith.com>
2014-09-29 14:42:10 -0700

    Auto-update.

--- hashes.sh
+++ hashes.sh
@@ -4,5 +4,5 @@ files=`gfind . -iregex '.*\(rb\|haml\)' -printf '%p\n'`
 for file in $files
 do
 	echo "file: $file"
-	gsed -i 's/\b\([a-z_]\+\):\{1\}\s\+\([a-z_0-9]*\)\b/:\1 => \2/g' $file
+	gsed -i 's/\b\([a-z_]\+\):\{1\}\s\+\([a-z_0-9""'']*\)\b/:\1 => \2/g' $file
 done
