035ad3e02d2a767ab89a818c877bf47706fc8f5a
Robin Smith <robin@mrrobinsmith.com>
2014-10-01 13:08:33 -0700

    Auto-update.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost', :port => 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
@@ -45,7 +45,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  # For Devise for some reason
-  config.action_mailer.default_url_options = { :host => 'localhost', :port => 3000 }
 end
