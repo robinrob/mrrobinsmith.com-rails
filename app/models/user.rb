192076218828ccd4aa1e283d4cd12bb907191437
Robin Smith <robin@mrrobinsmith.com>
2014-10-01 13:05:35 -0700

    Auto-update.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ require 'bcrypt'
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessor :encrypted_password
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 end
