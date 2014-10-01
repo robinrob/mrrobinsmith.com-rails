0c1aa6bba243e2c4957201441a9164c0dcc6d24e
Robin Smith <robin@mrrobinsmith.com>
2014-10-01 13:24:41 -0700

    Auto-update.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'smtp.gmail.com', :port => 587 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :host                 => "smtp.gmail.com",
+      :address                 => "smtp.gmail.com",
       :port                 => 587,
       :domain               => "mrrobinsmith.com",
       :user_name            => "robin@mrrobinsmith.com",
